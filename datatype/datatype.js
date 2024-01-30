7c5545875f829171041651887caa26d504a462bd
Anirudh Khade <158204983+Anirudhkhade@users.noreply.github.com>
2024-01-30 19:28:41 +0000

    datatype

---
+++ datatype/datatype.js
@@ -0,0 +1,13 @@
+
+"use strict"// treat all js code as newer version
+//alert(3+3) // we are using node js not browser
+
+// number => 2 to power 53
+//bigint
+//string =>""
+//boolean =>true/false
+//null => standalone value
+//undefined =>not defined or not known not assigned
+//symbol =>   
+//object
+console.log(typeof null);
